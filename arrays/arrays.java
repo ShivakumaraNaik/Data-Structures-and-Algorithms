8725951ee6a256c1e50aa265c4b108c23647984a
ShivakumaraNaik <shivushiva244@email.com>
2026-02-18 18:54:59 +0530

    Initial commit - Added DSA practice questions

---
+++ arrays/arrays.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class arrays {
+    public static int  lar(int num[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0;i<num.length;i++){
+            if (largest<num[i]) {
+                largest=num[i];   
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int num[] = {1,2,6,3,50};
+        System.out.println("largest value is "+ lar(num));
+    }
+}
