b8844da8015289e024df36cd1b858efb65f7232e
ShivakumaraNaik <shivushiva244@email.com>
2026-02-19 21:10:21 +0530

    updated with the sum of sub array

--- arrays/subarr.java
+++ arrays/subarr.java
@@ -2,21 +2,31 @@ import java.util.*;
 
 
 public class subarr {
-    public static void subar(int num[]){
+    public static void maxnumarr(int num[]){
+        int currentsum=0;
+        int maxsum=Integer.MIN_VALUE;
+
         for(int i=0;i<num.length;i++){
             int start=i;
             for(int j=i;j<num.length;j++){
+                currentsum=0;
                 int end=j;
                for(int k=start;k<=end;k++){
-                System.out.print(num[k]+" ");
+                currentsum += num[k];
+                
                }
-               System.out.println();
+               System.out.println(currentsum);
+               if (maxsum<currentsum) {
+                maxsum=currentsum;
+               }
+               
             }
-            System.out.println();
+           
         }
+        System.out.println("maximum sum is"+maxsum);
     }
     public static void main(String[] args) {
         int num[]={2,4,6,8};
-        subar(num);
+        maxnumarr(num);
     }
 }
